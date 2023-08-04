e4e026577f235473f40ccded15eaa438a3872eb0
Rajeev Ranjan <rajeevr@beelabs.technology>
2023-08-04 17:34:42 +0530

    UIs of HandEver

---
+++ app/src/main/java/com/example/handever/HandeverId_card_Fragment.kt
@@ -0,0 +1,19 @@
+package com.example.handever
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+ class HandeverId_card_Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_handever_id_card_, container, false)
+    }
+
+
+}
