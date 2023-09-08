66cb9004364927f0ab755cf7f49b84ada3e3ca2e
Rajeev Ranjan <rajeevr@beelabs.technology>
2023-09-08 19:43:49 +0530

    done

--- app/src/main/java/com/example/handever/Dashboard_Fragment.kt
+++ app/src/main/java/com/example/handever/Dashboard_Fragment.kt
@@ -1,18 +1,33 @@
 package com.example.handever
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 
 class Dashboard_Fragment : Fragment() {
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard_, container, false)
-    }
 
 
-    }
+
+
+     }
+
+ }
+
+
+
